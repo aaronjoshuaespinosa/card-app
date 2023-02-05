fc66e47eb2e9d8a0b7bf967bc5458559a5cadd61
AJ Espinosa <espinosa.aaron45@gmail.com>
2023-02-05 22:29:23 +0800

    Fixed login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -6,11 +6,11 @@ const Login = () => {
             <Head>
                 <title>Card App</title>
             </Head>
-            <nav className='fixed w-full h-auto bg-stone-900 text-slate-100 flex items-center justify-between py-[24px] px-[48px]'>
+            <nav className='fixed w-full h-auto flex items-center justify-between py-[24px] px-[48px]'>
                 <p className='font-bold text-4xl'>Card App</p>
                 <p>OMG WOW OMEGALUL GIGACHAD KEKW</p>
             </nav>
-            <main className='w-full h-screen bg-stone-900 text-slate-100 flex items-center justify-center'>
+            <main className='w-full h-screen flex items-center justify-center'>
                 <div className='flex flex-col gap-y-[12px]'>
                     <h1 className='font-bold text-center text-4xl'>log in</h1>
                     <h4 className='text-center text-lg'>input your credentials below.</h4>
