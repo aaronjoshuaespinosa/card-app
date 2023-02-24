b0c2bf00470d79f551e03372f83a8bb23585c897
AJ Espinosa <espinosa.aaron45@gmail.com>
2023-02-24 13:49:45 +0800

    Clean unused components

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,4 +1,4 @@
-import { FormInput, NavBar } from '@/components';
+import { FormInput } from '@/components';
 import Head from 'next/head'
 import Link from 'next/link';
 import { useRouter } from 'next/router';
